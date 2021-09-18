4b93b874f1449d8aeab3f0cc26c75b13fc62e32c
Ivan Herman <ivan@ivan-herman.net>
2021-09-18 09:04:46 +0200

    Filtering out templates

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -9,14 +9,11 @@ export namespace Constants {
     /** Location for the implementation reports */
     export const TEST_RESULTS_DIR: string = 'reports';
 
-    /** Location for the templates */
-    export const TEST_RESULTS_TEMPLATES_DIR: string = 'templates';
-
     /** Location for the final report */
     export const DOCS_DIR: string = 'results';
 
     /** Location for the implementation report templates */
-    export const TEST_RESULTS_TEMPLATE: string = `${TEST_RESULTS_DIR}/${TEST_RESULTS_TEMPLATES_DIR}/template.json`;
+    export const TEST_RESULTS_TEMPLATE: string = `${TEST_RESULTS_DIR}/xx-template.json`;
 
     /** Base URL for the tests in the repository */
     export const TEST_URL_BASE: string = 'https://github.com/iherman/epub-testing/tree/main/tests';
